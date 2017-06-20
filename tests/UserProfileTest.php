6fbda3ced7f452086e4cee3760fe8ca9c0a52eeb
Vongola <vongola12324@coder.tw>
2017-06-21 00:10:13 +0800

    Fix middleware issue when test

--- tests/UserProfileTest.php
+++ tests/UserProfileTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Role;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class UserProfileTest extends TestCase
@@ -7,23 +8,32 @@ class UserProfileTest extends TestCase
     use DatabaseMigrations;
 
     private $profile;
+    private $user;
+
 
     public function setUp()
     {
         parent::setUp();
         $this->profile = factory(App\Profile::class)->create();
+        $this->user = $this->profile->user;
+        $this->user->attachRole(Role::where('name', '=', 'Admin')->first());
     }
 
     public function testUserProfile()
     {
         $this->seeInDatabase('users', [
-            'id' => $this->profile->user->id,
+            'id' => $this->user->id,
         ]);
-        $this->assertEquals($this->profile->user->profile->id, $this->profile->id);
+        $this->assertEquals($this->user->profile->id, $this->profile->id);
     }
 
     public function testUserIndex()
     {
-        $this->get('/user')->seeRouteIs('user.index');
+        $this->actingAs($this->user)->get('/user')->seeRouteIs('user.index');
+    }
+
+    public function testUserShow()
+    {
+        $this->actingAs($this->user)->get('/user/'.$this->user->id)->seeRouteIs('user.show', $this->user->id);
     }
 }
